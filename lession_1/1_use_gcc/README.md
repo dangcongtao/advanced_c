9f9fe54f7b8886984953b10da0eba4f0dc808feb
dangcongtao <dangcongtao1999tn@gmail.com>
2023-11-02 22:39:42 +0700

    use gcc to compile, basic file

---
+++ lession_1/1_use_gcc/README.md
@@ -0,0 +1,5 @@
+# using to compile code file #
+
+# basic 
+1. create run file: gcc -o runfile src_file.c
+2. ./runfile to run file execute.
---
+++ lession_1/1_use_gcc/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define MAX 20
+
+int main(int argc, char const *argv[])
+{
+    printf(" hello world: %d \n", MAX);
+    return 0;
+}
