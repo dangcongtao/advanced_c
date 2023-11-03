ddb227e231091a6a5636479d7ecc47fad0da3e18
dangcongtao <dangcongtao1999tn@gmail.com>
2023-11-03 21:53:50 +0700

    use cmake with an example

---
+++ lession_2/README.md
@@ -0,0 +1,11 @@
+## MACRO ##
+
+# compile.
+- go to build folder
+- run: cmake .
+- make
+- ./main
+
+# content list 
+1. basic-macro.
+2. macro-function.
---
+++ lession_2/basic_macro.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#define MACRO_VALUE 100
+int main(int argc, char const *argv[])
+{
+    printf("this is macro value: %d \n", MACRO_VALUE);
+    return 0;
+}
